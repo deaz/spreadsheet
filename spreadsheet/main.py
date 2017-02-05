1f3d0bd52f17fe91e0128ccee5a78aa2d8964226
Ivan Vologdin <vologdin.nsk@gmail.com>
2017-02-06 00:14:26 +0700

    Add comments

--- spreadsheet/main.py
+++ spreadsheet/main.py
@@ -1,16 +1,20 @@
+import copy
 import operator
 import re
 from typing import Dict, Tuple
-import copy
 
 import spreadsheet.errors as errors
 from spreadsheet.cell import Cell, CellType
-from spreadsheet.utils import get_sorted_values, exit_with_error
+from spreadsheet.utils import (get_sorted_values, exit_with_error,
+                               get_operations_and_operands)
 
+# Sheet is dict of rows with keys counted from 1
+# and each row is dict of cells with keys of letters from 'A'
 Sheet = Dict[int, Dict[str, Cell]]
 
 
 def read_dimensions() -> Tuple[int, int]:
+    """Read dimensions of input sheet"""
     try:
         row_count, column_count = map(int, input().strip().split(' '))
     except ValueError:
@@ -21,10 +25,13 @@ def read_dimensions() -> Tuple[int, int]:
 
 
 def read_input(row_count, column_count) -> Sheet:
+    """Read sheet"""
     sheet = dict()
+    # Rows counted from 1
     for row_index in range(1, 1 + row_count):
         row = dict()
         try:
+            # Columns counted from 'A' letter
             for index, cell in enumerate(input().split('\t')):
                 column_letter = chr(ord('A') + index)
                 row[column_letter] = parse_cell(cell)
@@ -39,6 +46,7 @@ def read_input(row_count, column_count) -> Sheet:
 
 
 def parse_cell(cell: str) -> Cell:
+    """Parse cell string and return corresponding Cell object"""
     if not cell:
         return Cell(CellType.NONE, '')
     elif cell[0] == '\'':
@@ -54,12 +62,14 @@ def parse_cell(cell: str) -> Cell:
 
 
 def is_valid_expression(cell: str) -> bool:
+    """Check if cell string represents valid expression or not"""
     reference_cell_re = re.compile(
         r'^=([A-Za-z]\d|\d+)([-+/*]([A-Za-z]\d|\d+))*$')
     return bool(reference_cell_re.match(cell))
 
 
 def eval_sheet(sheet: Sheet) -> Sheet:
+    """Calculate sheet expressions"""
     evaluated = copy.deepcopy(sheet)
     for row_index, row in evaluated.items():
         for column_letter, cell in row.items():
@@ -70,7 +80,13 @@ def eval_sheet(sheet: Sheet) -> Sheet:
     return evaluated
 
 
+OPERATION_FUNCS = {'+': operator.add, '-': operator.sub,
+                   '*': operator.mul, '/': operator.floordiv}
+
+
 def eval_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
+    """Calculate specified cell of sheet"""
+
     cell_address = column + str(row)
     if cell_address in visited:
         return Cell(CellType.ERROR, errors.CIRCULAR_DEP)
@@ -81,34 +97,34 @@ def eval_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
     except KeyError:
         return Cell(CellType.ERROR, errors.NONEXISTENT_CELL)
 
+    # Return already evaluated cell
     if cell.type != CellType.EXPRESSION:
         return cell
 
-    term_re = re.compile(r'[A-Za-z]\d|\d+')
-    operation_re = re.compile(r'[-+/*]')
-    operation_funcs = {'+': operator.add, '-': operator.sub,
-                       '*': operator.mul, '/': operator.floordiv}
-    operands = term_re.findall(cell.value)
-    operations = operation_re.findall(cell.value)
+    operations, operands = get_operations_and_operands(cell)
     cell_value = 0
     # '+' is dummy operation for first operand
     for operation, operand in zip(['+'] + operations, operands):
         evaluated_cell = eval_term(operand, sheet, visited)
         value = evaluated_cell.value
         if evaluated_cell.type == CellType.ERROR:
+            # Set error message for cell
+            # if one of referenced cells contains error
             return evaluated_cell
         elif evaluated_cell.type == CellType.STRING:
             return Cell(CellType.ERROR, errors.WRONG_ARG)
         elif evaluated_cell.type == CellType.NONE:
+            # Use empty cell as 0 for expressions
             value = 0
         try:
-            cell_value = operation_funcs[operation](cell_value, value)
+            cell_value = OPERATION_FUNCS[operation](cell_value, value)
         except ZeroDivisionError:
             return Cell(CellType.ERROR, errors.ZERO_DIV)
     return Cell(CellType.NUMBER, cell_value)
 
 
 def eval_term(term: str, sheet: Sheet, visited: set) -> Cell:
+    """Calculate term of cell expression"""
     if term.isdigit():
         return Cell(CellType.NUMBER, int(term))
     else:
@@ -116,6 +132,7 @@ def eval_term(term: str, sheet: Sheet, visited: set) -> Cell:
 
 
 def format_sheet(sheet: Sheet) -> str:
+    """Format sheet as readable string"""
     result = ''
     sorted_rows = get_sorted_values(sheet)
     for row in sorted_rows:
--- spreadsheet/utils.py
+++ spreadsheet/utils.py
@@ -1,5 +1,8 @@
-from typing import Dict, List
+import re
 import sys
+from typing import Dict, List, Tuple
+
+from spreadsheet.cell import Cell
 
 
 def get_sorted_values(dictionary: Dict) -> List:
@@ -9,3 +12,11 @@ def get_sorted_values(dictionary: Dict) -> List:
 def exit_with_error(message: str) -> None:
     sys.stderr.write(message + '\n')
     sys.exit(1)
+
+
+def get_operations_and_operands(cell: Cell) -> Tuple[List[str], List[str]]:
+    operation_re = re.compile(r'[-+/*]')
+    operand_re = re.compile(r'[A-Za-z]\d|\d+')
+    operations = operation_re.findall(cell.value)
+    operands = operand_re.findall(cell.value)
+    return operations, operands
