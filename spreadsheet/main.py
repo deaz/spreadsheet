9b74bf2756e1fd4d4c5cbb2528d56dc084722e0a
Ivan Vologdin <vologdin.nsk@gmail.com>
2017-02-05 19:43:01 +0700

    Change some identifier names

--- spreadsheet/main.py
+++ spreadsheet/main.py
@@ -59,17 +59,17 @@ def is_valid_expression(cell: str) -> bool:
 
 
 def eval_sheet(sheet: Sheet) -> Sheet:
-    computed_sheet = copy.deepcopy(sheet)
-    for row_index, row in computed_sheet.items():
+    evaluated = copy.deepcopy(sheet)
+    for row_index, row in evaluated.items():
         for column_letter, cell in row.items():
             if cell.type == CellType.EXPRESSION:
                 visited = set()
-                row[column_letter] = compute_cell(
-                    computed_sheet, row_index, column_letter, visited)
-    return computed_sheet
+                row[column_letter] = eval_cell(
+                    evaluated, row_index, column_letter, visited)
+    return evaluated
 
 
-def compute_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
+def eval_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
     cell_address = column + str(row)
     if cell_address in visited:
         return Cell(CellType.ERROR, errors.CIRCULAR_DEP)
@@ -92,13 +92,13 @@ def compute_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
     cell_value = 0
     # '+' is dummy operation for first operand
     for operation, operand in zip(['+'] + operations, operands):
-        computed_cell = compute_term(operand, sheet, visited)
-        value = computed_cell.value
-        if computed_cell.type == CellType.ERROR:
-            return computed_cell
-        elif computed_cell.type == CellType.STRING:
+        evaluated_cell = eval_term(operand, sheet, visited)
+        value = evaluated_cell.value
+        if evaluated_cell.type == CellType.ERROR:
+            return evaluated_cell
+        elif evaluated_cell.type == CellType.STRING:
             return Cell(CellType.ERROR, errors.WRONG_ARG)
-        elif computed_cell.type == CellType.NONE:
+        elif evaluated_cell.type == CellType.NONE:
             value = 0
         try:
             cell_value = operation_funcs[operation](cell_value, value)
@@ -107,11 +107,11 @@ def compute_cell(sheet: Sheet, row: int, column: str, visited: set) -> Cell:
     return Cell(CellType.NUMBER, cell_value)
 
 
-def compute_term(term: str, sheet: Sheet, visited: set) -> Cell:
+def eval_term(term: str, sheet: Sheet, visited: set) -> Cell:
     if term.isdigit():
         return Cell(CellType.NUMBER, int(term))
     else:
-        return compute_cell(sheet, int(term[1]), term[0].upper(), visited)
+        return eval_cell(sheet, int(term[1]), term[0].upper(), visited)
 
 
 def format_sheet(sheet: Sheet) -> str:
@@ -127,8 +127,8 @@ def format_sheet(sheet: Sheet) -> str:
 def main() -> None:
     row_count, column_count = read_dimensions()
     sheet = read_input(row_count, column_count)
-    computed_sheet = eval_sheet(sheet)
-    print(format_sheet(computed_sheet))
+    evaluated_sheet = eval_sheet(sheet)
+    print(format_sheet(evaluated_sheet))
 
 
 if __name__ == '__main__':
